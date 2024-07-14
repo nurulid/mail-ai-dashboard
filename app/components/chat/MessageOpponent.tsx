12f1a65209cb8f35a7a6559a0c9f732d9083f3b5
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-07-14 21:56:49 +0700

    update message opponent

--- app/components/chat/MessageOpponent.tsx
+++ app/components/chat/MessageOpponent.tsx
@@ -5,12 +5,14 @@ import {
   EllipsisVertical,
   FileText,
   Map,
+  Share,
   Share2,
 } from "lucide-react";
 import React from "react";
 
 import { Avatar } from "../ui/Avatar";
 import { Checkbox } from "../ui/Checkbox";
+import Image from "next/image";
 
 const DateCard = () => {
   return (
@@ -99,49 +101,65 @@ const AttachmentCard = () => {
 
 export const MessageOpponent = () => {
   return (
-    <div className="flex items-end justify-start gap-5 ml-4 mt-8">
-      <Avatar size="xl" name="JCD.AI" />
+    <div className="flex items-end justify-start gap-5 ml-4 mt-8 mb-20">
+      <Avatar size="xl" name="A I" />
       <div className="flex items-end gap-3">
-        <div className="bg-gray-30 rounded-xl rounded-bl-none text-gray-300 max-w-[690px] w-full border-[10px] border-gray-30">
-          <div className="rounded-md bg-white py-4 px-8">
-            <h2 className="mb-[10px] font-medium text-gray-800">
-              Meeting with a client
-            </h2>
-            <p className="mb-[10px]">
-              “FitMe” is a leading catering company that specializes in
-              delivering daily, healthy food options.
-            </p>
-            <ul>
-              <li className="py-3 border-b border-gray-50">
-                <Checkbox label="Product presentation" id="check1" />
-              </li>
-              <li className="py-3 border-b border-gray-50">
-                <Checkbox
-                  label="Prototype or product demonstration"
-                  id="check2"
-                />
-              </li>
-              <li className="py-3 border-b border-gray-50">
-                <Checkbox label="Project portfolio and contract " id="check3" />
-              </li>
-            </ul>
+        <div className="translate-y-10 space-y-[10px]">
+          <div className="bg-gray-30 rounded-xl rounded-bl-none text-gray-300 max-w-[690px] w-full border-[10px] border-gray-30">
+            <div className="rounded-md bg-white py-4 px-8">
+              <div className="flex gap-5 items-start">
+                <div>
+                  <h2 className="mb-[10px] font-medium text-gray-800">
+                    Meeting with a client
+                  </h2>
+                  <p className="mb-[10px]">
+                    “FitMe” is a leading catering company that specializes in
+                    delivering daily, healthy food options.
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <span className="text-xs text-gray-300">Client:</span>
+                  <div className="bg-gray-30 py-[10px] px-4 rounded-md">
+                    <Image src="/logo-fitme.svg" width={150} height={19} alt="Client logo"/>
+                  </div>
+                </div>
+              </div>
+              <ul>
+                <li className="py-3 border-b border-gray-50">
+                  <Checkbox label="Product presentation" id="check1" />
+                </li>
+                <li className="py-3 border-b border-gray-50">
+                  <Checkbox
+                    label="Prototype or product demonstration"
+                    id="check2"
+                  />
+                </li>
+                <li className="py-3 border-b border-gray-50">
+                  <Checkbox label="Project portfolio and contract " id="check3" />
+                </li>
+              </ul>
+              <div className="grid grid-cols-3 gap-2 mt-3">
+                <div>
+                  <DateCard />
+                </div>
+                <div className="col-span-2">
+                  <MapCard />
+                </div>
+              </div>
+            </div>
+            {/* attachments */}
             <div className="grid grid-cols-3 gap-2 mt-3">
               <div>
-                <DateCard />
+                <AttachmentCard />
               </div>
               <div className="col-span-2">
-                <MapCard />
+                <AttachmentCard />
               </div>
             </div>
           </div>
-          {/* attachments */}
-          <div className="grid grid-cols-3 gap-2 mt-3">
-            <div>
-              <AttachmentCard />
-            </div>
-            <div className="col-span-2">
-              <AttachmentCard />
-            </div>
+          <div className="flex justify-end gap-2">
+            <button className="text-xs py-2 px-5 rounded-full flex items-center justify-center gap-2 bg-white text-gray-300"><Copy className="inline-block" size={18}/> Copy</button>
+            <button className="text-xs py-2 px-5 rounded-full flex items-center justify-center gap-2 bg-white text-gray-300"><Share className="inline-block" size={18}/> Share</button>
           </div>
         </div>
         <span className="text-sm text-gray-300">11:02</span>
