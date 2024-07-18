b383e72dacd6dafb0ac542fe9e84257ceca4ba4e
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-07-18 12:31:21 +0700

    improve button

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -1,43 +1,25 @@
-import React, { ComponentProps, PropsWithChildren } from "react";
+import { ComponentProps, PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
-type Color = "primary" | "secondary";
+const variants = {
+  primary: "bg-accent-blue text-white border-accent-blue",
+  secondary: "bg-white border",
+};
 
-interface ButtonProps 
-  extends PropsWithChildren<ComponentProps<"button">> {
-    color?: Color;
-    isFull?: boolean;
-    className?: string;
-    type?: "button" | "submit" | "reset";
+interface ButtonProps extends ComponentProps<"button"> {
+  variant?: keyof typeof variants;
+  isFull?: boolean;
 }
 
+const buttonClassesBase: string[] = ["px-7 rounded-md text-sm font-medium transition-all flex justify-center items-center gap-2"];
+
 export const Button = (props: PropsWithChildren<ButtonProps>) => {
-  const {
-    color = "primary",
-    isFull = false,
-    className = "",
-    type = "button",
-    children,
-    ...buttonProps
-  } = props;
-
-  const variants: Record<Color, string> = {
-    primary: "bg-accent-blue text-white border-accent-blue",
-    secondary: "bg-white border",
-  };
-
+  const { className = "", variant = "primary", isFull = false, children, ...buttonProps } = props;
+  const btnWidth = isFull ? "w-full py-[10px]" : "py-3";
   return (
     <button
+      className={twMerge(buttonClassesBase, btnWidth, className, variants[variant])}
       {...buttonProps}
-      type={type}
-      className={[
-        "px-7 rounded-md text-sm font-medium transition-all",
-        "flex justify-center items-center gap-2",
-        isFull ? "w-full py-[10px]" : "py-3",
-        className,
-        variants[color],
-      ].join(" ")}
-    >
-      {children}
-    </button>
+    >{children}</button>
   );
 };
