a54d03e9977c64e8f9336eeceb3b2881e6841bb3
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-08-26 23:25:05 +0700

    notif and avatar label

--- app/components/ui/Avatar.tsx
+++ app/components/ui/Avatar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import { withUnit } from "@/app/utils";
+import { Dot } from "lucide-react";
 
 const sizes = { xl: 58, lg: 40, md: 38, sm: 32 };
 
@@ -15,25 +16,28 @@ export const Avatar = (props: AvatarProps) => {
   const size = sizes[props.size];
   const sizePx = withUnit(+size, "px");
   return (
-    <div
-      className="rounded-full overflow-hidden relative group bg-white"
-      style={{ width: sizePx, height: sizePx }}
-    >
-      {imageUrl ? (
-        <Image alt="" height={size} width={size} src={imageUrl} />
-      ) : (
-        <div className="w-full h-full flex justify-center items-center font-bold bg-accent-red text-xs text-white">
-          {name
-            .split(" ")
-            .map((letter) => letter.slice(0, 1))
-            .slice(0, 2)
-            .map((letter, index) => (
-              <span key={index} className="capitalize">
-                {letter}
-              </span>
-            ))}
-        </div>
-      )}
+    <div className="relative">
+      <div
+        className="rounded-full overflow-hidden group bg-white"
+        style={{ width: sizePx, height: sizePx }}
+      >
+        {imageUrl ? (
+          <Image alt="" height={size} width={size} src={imageUrl} />
+        ) : (
+          <div className="w-full h-full flex justify-center items-center font-bold bg-accent-red text-xs text-white">
+            {name
+              .split(" ")
+              .map((letter) => letter.slice(0, 1))
+              .slice(0, 2)
+              .map((letter, index) => (
+                <span key={index} className="capitalize">
+                  {letter}
+                </span>
+              ))}
+          </div>
+        )}
+      </div>
+      <Dot className="absolute top-1 -right-4 z-10" size={40} color="#49DD9F" />
     </div>
   );
 };
--- app/components/ui/Header.tsx
+++ app/components/ui/Header.tsx
@@ -1,9 +1,9 @@
-import { Bell, ChevronDown } from 'lucide-react'
-import React from 'react'
+import { Bell, ChevronDown, Dot } from "lucide-react";
+import React from "react";
 
-import { useUser } from '@/app/hooks/useUser'
+import { useUser } from "@/app/hooks/useUser";
 
-import { Avatar } from './Avatar';
+import { Avatar } from "./Avatar";
 
 export const Header = () => {
   const userData = useUser();
@@ -11,17 +11,21 @@ export const Header = () => {
     <header className="flex justify-end items-center py-5 px-6">
       <div className="relative text-gray-200">
         <Bell />
-        <span className="notif__label"></span>
+        <Dot
+          className="absolute -top-[14px] -left-[3px]"
+          size={40}
+          color="#EF655F"
+        />
       </div>
       <div className="user flex items-center gap-3 px-4 ml-4 border-l-2">
-        <Avatar size="sm" name={userData.name} imageUrl={userData.avatar}/>
+        <Avatar size="sm" name={userData.name} imageUrl={userData.avatar} />
         <div>
-          <h3 className="text-sm text-gray-600 mr-1 inline-block">
+          <h3 className="text-sm text-gray-600 mr-1 inline-block relative">
             {userData.name}
           </h3>
           <ChevronDown size={14} className="inline" />
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
