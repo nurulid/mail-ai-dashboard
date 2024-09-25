d21a73e4467aca8a327aa2da115e8f08a8e230c4
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-09-26 01:01:41 +0700

    test

--- app/(pages)/chat/layout.tsx
+++ app/(pages)/chat/layout.tsx
@@ -1,8 +1,11 @@
+"use client"
+
 import React from 'react'
 
 import { ChatLayout } from '@/app/components/layout/ChatLayout'
 
 export default function Layout({children}: React.PropsWithChildren) {
+
   return (
     <ChatLayout>{children}</ChatLayout>
   )
--- app/components/layout/ChatLayout.tsx
+++ app/components/layout/ChatLayout.tsx
@@ -11,7 +11,12 @@ import { MessageInput } from "../chat/MessageInput";
 import { Navigation } from "../chat/Navigation";
 import { Header } from "../ui/Header";
 
-export const ChatLayout = ({ children }: React.PropsWithChildren) => {
+interface ChatLayoutProps {
+  chatTitle?: string;
+}
+
+export const ChatLayout = (props: React.PropsWithChildren<ChatLayoutProps>) => {
+  const { chatTitle, children } = props;
   const { isDetailsOpen } = useMessageDetails();
   return (
     <section className="flex w-full h-full">
@@ -21,7 +26,7 @@ export const ChatLayout = ({ children }: React.PropsWithChildren) => {
         <div className="flex flex-col h-[calc(100vh-72px)] pr-3">
           <div className="flex gap-3">
             <div className="bg-[#F1E8F5] rounded-xl p-[10px] h-[calc(100vh-160px)] overflow-auto relative flex-1 w-auto">
-              <HeaderChat />
+              <HeaderChat title={chatTitle}/>
               {children}
             </div>
             <AnimatePresence>{isDetailsOpen && <Details />}</AnimatePresence>
